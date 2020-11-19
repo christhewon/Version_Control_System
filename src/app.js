999c7890cafbb1398d7b998cd40ee4b768a92d8a
christhewon <chrislee752@yahoo.com>
2020-11-19 09:22:44 -0800

    List Command noe functional

--- src/app.js
+++ src/app.js
@@ -19,12 +19,12 @@ const ejs = require('ejs');
 
 
 //var $ = require('jquery');
-var app = express();  // Init an Express object.
+let app = express();  // Init an Express object.
 
-app.set('views', './views');
+// app.set('views', './views');
 app.set('view engine', 'ejs');
 app.get('/', function (req, res) { // Set page-gen fcn for URL root request.
-    res.render('index', { root : __dirname});
+    res.render('index');
     //res.sendFile('javascript.js', { root : __dirname});
 });
 //app.use('/js', express.static(__dirname + './javascript.js'));
@@ -43,6 +43,10 @@ let hour = 0;
 let minute = 0;
 let second = 0;
 let manifestCount = 1;
+let labels = [];
+
+global.manifestLabels = [];
+global.testVar = "Test Variable";
 
 
 
@@ -70,8 +74,6 @@ app.get('/userCmd', (req, res) => {
     let directories = [];
 
     if (command == "crRepo") {
-
-        console.log(userInput);
         let startDirectory = splitInput[1];
         let endDirectory = splitInput[2];
         createRepo(startDirectory, endDirectory, directories);
@@ -87,21 +89,48 @@ app.get('/userCmd', (req, res) => {
     }
     else if (command == "list") {
         let repoLocation = splitInput[1]
-        var labels = List(repoLocation)
+        let manifestName = splitInput[2]
+        labels = List(repoLocation, manifestName)
 
-        res.render('index', {manifestLabels: ['test1', 'test2', 'test3']});
-        //res.render('index', {manifestLabels:  labels});
+
+        console.log("Right before Render")
+        //res.render('index', {manifestLabels: ['test1', 'test2', 'test3']});
+        res.render('index', {manifestLabels:  labels});
+        console.log("Render Complete")
         //res.render("index", {"variable": labels})
     }
-    res.render('index', {hello: "hello"});
+    else if (command == "checkIn") {
+        let startDirectory = splitInput[1];
+        let endDirectory = splitInput[2];
+        checkIn(startDirectory, endDirectory, directories);
+        res.render('index');
+    }
+    else if (command == "checkOut") {
+        let manifestDir = splitInput[1]
+        let endDirectory = splitInput[2]
+        checkOut(manifestDir, endDirectory);
+    }
+
 
 });
+
+app.get("/", function(req, res){
+    var hello = "Hello"
+    res.render("index.ejs", {exampleVar: hello});
+});
+
+/*
+app.get('/userQuery', (req, res) => {
+    res.render('index', {data : {userQuery: req.params.userQuery}})
+});
+
+ */
 
 // Adds a label file for manifest files
 function Label(label, repoLocation, manifestName) {
     console.log("Inside Label method")
     let fileNames = fs.readdirSync(repoLocation);
-    let labelAbsolutePath = repoLocation.concat("\\", "label.txt");
+    let labelAbsolutePath = repoLocation.concat("\\", "label", manifestName, ".txt");
     if (!fs.existsSync(labelAbsolutePath)) {
         console.log("It does not exist")
         // Creates new label file  with the first line containing the manifestFile's name
@@ -109,35 +138,109 @@ function Label(label, repoLocation, manifestName) {
     }
     // Adds the new label for the manifest file to the label file
     fs.appendFileSync(labelAbsolutePath, label.concat("\n"));
-
-    fileNames.forEach(function (file) {
-
-    });
 }
 
-function List(repoLocation, response) {
-    let absolutePath = repoLocation.concat("\\", "label.txt")
-    let labels = getLabels(absolutePath)
-    for (let i = 0; i < labels.length; i++) {
+function List(repoLocation, manifestName) {
+    let absolutePath = repoLocation.concat("\\", "label.", manifestName)
+    let arr = [];
 
-    }
-    return labels;
+    // Getting the contents of the text file
+    let contents = fs.readFileSync(absolutePath, 'utf-8');
+
+    // Splitting each line
+    arr = contents.split("\n")
+
+    // Getting rid of empty index
+    arr.pop();
+    return arr;
 }
 
+/*
 // returns manifest file name as well as labels for the manifest file
 function getLabels(fileAbsolutePath) {
+    console.log("Inside getLabels")
     let labels = [] // Can i use 'let' in this case cause its getting returned
     let readInterface = readline.createInterface({
         input: fs.createReadStream(fileAbsolutePath),
         output: process.stdout,
         console: false
     });
+    console.log("read interface create done")
 
     // adds all labels to array, including the manifest file name on the first line
     readInterface.on('line', function(line) {
         labels.push(line)
+        console.log("Line pushed: ", line)
     });
+    
+    console.log("Right before Return")
     return labels
+}
+*/
+
+
+function checkOut(manifestDir, endDir) {
+    let lines = [];
+    let files = [];
+    let artId = [];
+    let relPath = [];
+    let readInterface = readline.createInterface({
+        input: fs.createReadStream(manifestDir),
+        output: process.stdout,
+        console: false
+    });
+
+    // adds all labels to array, including the manifest file name on the first line
+    readInterface.on('line', function(line) {
+        lines.push(line);
+    });
+
+    setTimeout(function () {
+        readInterface.close();
+        readInterface.removeAllListeners();
+    }, 4000);
+
+
+    console.log("finished readInterface");
+
+    for(let i = 0; i < lines.length; i++) {
+        console.log("In first loop");
+        if (lines[i].substring(0,1) == "P") {
+            files.push(lines[i])
+        }
+    }
+
+    for(let i = 0; i < files.length; i++) {
+        let current = files[i].split(",")
+        artId.push(current[0])
+        relPath.push(current[1])
+    }
+
+    for(let i = 0; i < artId.length; i++) {
+        let absoluteFilePath = endDir.concat(relPath[i])
+        let e1 = manifestDir;
+        let e2 = manifestDir.split("\\");
+        console.log(e2);
+
+        let oldAbsolutePath = ""
+        for(let i = 0; i < e2.length - 1; i++) {
+            oldAbsolutePath.concat(e2[i], "\\")
+        }
+
+        fse.copySync(oldAbsolutePath, absoluteFilePath);
+    }
+}
+
+function checkIn(startDir, endDir, dir) {
+    clearFiles(endDir)
+
+    setTimeout(function () {
+        copyWithArtID(startDir, endDir, dir)
+    }, 1000);
+
+    setTimeout(function () {
+        createManifest(endDir)
+    }, 2000);
 }
 
 
@@ -147,13 +250,102 @@ function createRepo(startDir, endDir, dir) {
         copyWithArtID(startDir, endDir, dir)
     }, 1000);
 
+
+
     setTimeout(function () {
         createManifest(endDir)
     }, 2000);
 
 }
 
+function clearFiles(folder) {
+    const fileNames = fs.readdirSync(folder); // C//User//Desktop//CopyThis
+    let relativePath = "";
 
+    // Iterates through each item in the startFolder
+    fileNames.forEach(function (file) {
+        let fileDirectory = folder.concat("\\", file);
+        let stats = fs.statSync(fileDirectory); //stats is the stats of the oldAbsolutePath
+
+        if (stats.isFile() == true && file.substring(0,1).localeCompare(".") != 0) {
+            fs.unlinkSync(fileDirectory)
+        }
+    });
+
+}
+
+function copyWithArtID(startFolder, endFolder, directories) { //startFolder the location of the file we want to walk through
+    const fileNames = fs.readdirSync(startFolder); // C//User//Desktop//CopyThis
+    let relativePath = "";
+
+    // Iterates through each item in the startFolder
+    fileNames.forEach(function (file) {
+
+        //the path or directory that led to the original project tree file
+        let oldAbsolutePath = startFolder.concat("\\", file);
+        console.log("file is: ", file);
+        let stats = fs.statSync(oldAbsolutePath); //stats is the stats of the oldAbsolutePath
+
+        // Check if it is a file and it is not a dot file (manifest file)
+        if (stats.isFile() == true && file.substring(0,1).localeCompare(".") != 0) {
+
+            // Gets the extension name of the file at oldAbsolutePath
+            let extension = path.extname(oldAbsolutePath);
+
+            //contents of the file
+            const content = fs.readFileSync(oldAbsolutePath, 'utf8');
+
+            // Creates the artifact id for the file
+            let artId = getArtifactID(content, oldAbsolutePath, startFolder);
+
+            // adds artIds to list used for createManifest
+            artIds.push(artId);
+
+            relativePath = oldAbsolutePath;
+            let r1 = relativePath.split('\\\\');
+            let r2 = r1[r1.length - 1]
+            let r3 = r2.split('\\');
+
+            let r4 ="";
+            for (let i = 1; i < r3.length; i++) {
+                r4 = r4.concat(r3[i], "\\");
+            }
+
+            relativePaths.push(r4); // adds relative path names to list used for createManifest
+
+            // The path or directory leading directly to the new file
+            let newAbsolutePath = endFolder.concat("\\", artId, extension);
+            fse.copySync(oldAbsolutePath, newAbsolutePath);
+        }
+
+        // Recursive call if there is another folder in the current directory
+        //Recursive call may make directories not work for certain files
+        if (stats.isDirectory() == true) {
+            /*
+            directories.push(file);
+            console.log("directories: ", directories);
+
+             */
+
+
+            copyWithArtID(oldAbsolutePath, endFolder, directories);
+            /*
+            if (directories.length > 0) {
+                console.log("Directories is greater than 0")
+                for ( i = 0; i < directories.length; i++) {
+                    relativePath = relativePath + "\\" + directories[i];
+                    console.log("Relative Path is now: ", relativePath);
+                }
+            }
+
+             */
+        }
+
+    });
+}
+
+
+/*
 function copyWithArtID(startFolder, endFolder, directories) { //startFolder the location of the file we want to walk through
     const fileNames = fs.readdirSync(startFolder); // C//User//Desktop//CopyThis
     let relativePath = "";
@@ -195,6 +387,9 @@ function copyWithArtID(startFolder, endFolder, directories) { //startFolder the 
             directories.push(file);
             console.log("directories: ", directories);
 
+
+            copyWithArtID(oldAbsolutePath, endFolder, directories);
+
             if (directories.length > 0) {
                 console.log("Directories is greater than 0")
                 for ( i = 0; i < directories.length; i++) {
@@ -202,11 +397,12 @@ function copyWithArtID(startFolder, endFolder, directories) { //startFolder the 
                     console.log("Relative Path is now: ", relativePath);
                 }
             }
-            copyWithArtID(oldAbsolutePath, endFolder, directories);
         }
 
     });
 }
+
+ */
 
 function createManifest(directory) {
     //var manifestDirectory = fs.appendFile(directory.concat("\\", ".manifest.txt"), "")
@@ -216,12 +412,12 @@ function createManifest(directory) {
 
     // Creating the data that will preside inside the manifest file
     let manifestData = "1) User Input: ".concat(userCommands[0], "\n", "2) Date: ",month.toString(), "-", day.toString(), "-", year.toString()," ", "Time: ",
-        hour.toString(), ":", minute.toString(), ":", second.toString(), "\n","3) ");
+        hour.toString(), ":", minute.toString(), ":", second.toString(), "\n","3) Files", "\n");
     console.log(artIds);
 
     // Adding each Artifact Id and its relative path in the project source tree
     for (i = 0; i < artIds.length; i++) {
-        manifestData = manifestData.concat("Artifact Id: ", artIds[i], " ", "Relative Path: ", relativePaths[i], "\n");
+        manifestData = manifestData.concat(artIds[i], ",", relativePaths[i], "\n");
     }
 
     // Wrties the data into the manifest file
@@ -259,5 +455,25 @@ function getCheckSum(String) {
     return (checkSum % 10000)
 }
 
+module.exports = { manifestLabels: labels }
 
 
+// Ex commands
+// crRepo C:\\Users\\Christopher\\Desktop\\CopyThis C:\\Users\\Christopher\\Desktop\\CopyMade
+// list C:\Users\Christopher\Desktop\CopyMade man-1.txt
+// label anotherTestLabel C:\Users\Christopher\Desktop\CopyMade .man-1
+
+
+/*
+createRepo copied all the files in your source folder to a destination (the repo), then it made a manifest file that
+ lists all the files it copied over. Now all those files in the repo are copies of some source file,
+ except they've been renamed with artifactIDs. This is all recorded in the manifest file.
+check-out takes a manifest file that you specify (since you will have many) and parses through to find all the
+artifactID files listed. Since the man file also specifies WHERE each of these files will go (the relative path),
+check-out can then recreate a source folder based on the contents listed in the man file -- into another destination
+
+so one is
+source -> repo
+ and the other is
+repo -> different source
+ */
